e09774a9171a1147ff01c54af4b34bb6e7722239
Virelion <m.ziemba95@gmail.com>
2017-11-25 18:28:16 +0100

    Provide ehnanced project structure
    Added release task.
    Renamed artifacts of impl and api modules.
    Provided dummy test cases.
    Changed file header.
    Provided simple javadoc to api module classes.

---
+++ impl/src/main/java/io2017/pierogimroku/task/ORMLiteTaskManager.java
@@ -0,0 +1,15 @@
+package io2017.pierogimroku.task;
+
+import io2017.pierogimroku.task.api.TaskManager;
+import io2017.pierogimroku.task.api.TaskView;
+
+/*
+ * Created by Pierogi Mroku, IO WIMiIP AGH 2017
+ */
+
+/**
+ * Implementation of task container
+ */
+public class ORMLiteTaskManager implements TaskManager, TaskView {
+
+}
